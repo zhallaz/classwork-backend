f62cfe80bf442926909293409650035087e5721d
ezejichinemerem <ezejichinemerem34@gmail.com>
2026-06-27 13:23:06 +0300

    updated product

--- controller/product.js
+++ controller/product.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product.model.js");
+const Product = require("../models/product.Model.js");
 const User = require("../models/user.models.js");
 exports.createProduct = async (req, res) => {
   try {
